f44b9c5ff868cb16c5d3d107add4e165825a9806
Ali Siddiqi <alisiddiqi01@gmail.com>
2021-05-09 17:14:00 -0500

    Updates

---
+++ precache-manifest.7f7fd2df3b15d24f6cf24ec29dc71892.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9183a5049a57ea91dc7ff4afe658b09e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "20924085d280f47ebef5",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "cfde32d4cdc657772252",
+    "url": "/home/static/js/2.0753cd9f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.0753cd9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20924085d280f47ebef5",
+    "url": "/home/static/js/main.be1c308a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "265e9f25ea93e6474c18f15eabaa34b1",
+    "url": "/home/static/media/aliPic.265e9f25.png"
+  },
+  {
+    "revision": "db499594ce600a01dfefefbc22a5cc98",
+    "url": "/home/static/media/resume.db499594.pdf"
+  }
+]);
