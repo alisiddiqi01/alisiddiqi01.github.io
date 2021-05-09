8b83072e05a074ba20aff73fccc2bc3cb20dd2a9
Ali Siddiqi <alisiddiqi01@gmail.com>
2021-05-09 17:16:23 -0500

    Updates

---
+++ precache-manifest.b1a39e87f5339d1f3135984e7dd928f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53995dabba73b516adff5808487a8e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96da9498a1d74e5498b9",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "cfde32d4cdc657772252",
+    "url": "/static/js/2.0753cd9f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0753cd9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96da9498a1d74e5498b9",
+    "url": "/static/js/main.bd9d9d17.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "265e9f25ea93e6474c18f15eabaa34b1",
+    "url": "/static/media/aliPic.265e9f25.png"
+  },
+  {
+    "revision": "db499594ce600a01dfefefbc22a5cc98",
+    "url": "/static/media/resume.db499594.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.7f7fd2df3b15d24f6cf24ec29dc71892.js"
+  "/precache-manifest.b1a39e87f5339d1f3135984e7dd928f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
